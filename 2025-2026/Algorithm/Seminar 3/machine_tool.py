2db10d6c7024311b97d9579197225be9e123fd41
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2025-08-31 03:19:33 +0300

    release 0

---
+++ 2025-2026/Algorithm/Seminar 3/machine_tool.py
@@ -0,0 +1,11 @@
+import time
+
+while True:
+    print('Кладем заготовку')
+    time.sleep(2)
+    print('Подводим шлифовальный круг')
+    time.sleep(2)
+    print('Шлифуем заготовку')
+    time.sleep(3)
+    print('Выталкиваем готовую деталь')
+    time.sleep(1)
