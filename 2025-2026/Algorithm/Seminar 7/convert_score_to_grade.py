0e86e2db9f44962336a7c641183d7c6aea2a8d25
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2025-09-26 00:29:24 +0300

    release 0

---
+++ 2025-2026/Algorithm/Seminar 7/convert_score_to_grade.py
@@ -0,0 +1,30 @@
+def score_to_grade(score):
+    """
+    Конвертирует числовую оценку в буквенную
+    """
+    if score < 0 or score > 100:
+        return "Некорректная оценка"
+
+    step = 100 // 4
+
+    if score > 100 - step:
+        grade = "A"
+        comment = "Отлично"
+    elif score > 100 - 2 * step:
+        grade = "B"
+        comment = "Хорошо"
+    elif score > 100 - 3 * step:
+        grade = "C"
+        comment = "Удовлетворительно"
+    else:
+        grade = "D"
+        comment = "Неудовлетворительно"
+
+    return f"Оценка: {grade} ({comment})"
+
+
+# Примеры использования
+print(score_to_grade(95))  # Оценка: A (Отлично)
+print(score_to_grade(74))  # Оценка: B (Хорошо)
+print(score_to_grade(40))  # Оценка: C (Удовлетворительно)
+print(score_to_grade(20))  # Оценка: D (Неудовлетворительно)
