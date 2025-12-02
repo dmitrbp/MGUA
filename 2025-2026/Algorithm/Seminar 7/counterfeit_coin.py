8bbf0f8c28f577ea47a4205e4f3bc28fbc8a8b7e
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2025-12-02 18:18:12 +0300

    release 0

--- 2025-2026/Algorithm/Seminar 7/counterfeit_coin.py
+++ 2025-2026/Algorithm/Seminar 7/counterfeit_coin.py
@@ -10,18 +10,26 @@ def check_counterfeit_coin():
     coins_def = [10, 10, 5]
     # Генерация случайного набора из начальных 3-х монет
     coins_set = random.sample(coins_def, 3)
+    # Создание словаря из массива, чтобы добавить индексы (ключи)
     coins_dict = {i: coins_set[i] for i in range(0, len(coins_set))}
     print(f"coins_set = {coins_set}")
     print(f"coins_dict = {coins_dict}")
+    # Выбираем случайным образом монету (ее индекс в словаре)
     index = random.randint(0, 2)
+    # Вынимаем монету и удаляем ее из словаря
     index_value = {index:coins_dict.pop(index)}
     print(f"Случайно выбранная монета - {index_value}")
+    # Оставшиеся в словаре ключи. В словаре осталось два элемента
     keys = list(coins_dict.keys())
 
+    # Взвешиваем оставшиеся в словаре две монеты
+    # Монеты равны по весу - вынутая монета фальшивая
     if coins_dict[keys[0]] == coins_dict[keys[1]]:
         print(f"Монета {index + 1} фальшивая")
+    # Первая монета легче второй - первая фальшивая
     elif coins_dict[keys[0]] < coins_dict[keys[1]]:
         print(f"Монета {keys[0] + 1} фальшивая")
+    # Вторая монета - фальшивая
     else:
         print(f"Монета {keys[1] + 1} фальшивая")
 
