c3c26e8197094b8692346b16d0d5d98141b12a84
dmitrbp <80584067+dmitrbp@users.noreply.github.com>
2026-03-18 01:02:18 +0300

    release 0

--- 2025-2026/Algorithm/Seminar 13/fibonacci.py
+++ 2025-2026/Algorithm/Seminar 13/fibonacci.py
@@ -1,5 +1,5 @@
+import time
 from sys import setrecursionlimit
-
 
 def fibonacci(n):
     # Базовые случаи
@@ -9,9 +9,26 @@ def fibonacci(n):
         return 1
     # Рекурсивный случай
     f = fibonacci(n - 1) + fibonacci(n - 2)
-    print(f)
     return f
 
+def fibonacci_memo(n, memo={}):
+    # Проверяем, считали ли мы уже это число
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    # Сохраняем результат перед возвратом
+    memo[n] = fibonacci_memo(n - 1, memo) + fibonacci_memo(n - 2, memo)
+    return memo[n]
+
+
 # setrecursionlimit(10000)
-f1 = fibonacci(20)
-print(f1)
+start_time = time.time()
+f1 = fibonacci(40)
+end_time = time.time()
+print(f"f1 = {f1}, Elapsed: {(end_time - start_time):.8f} seconds")
+
+start_time = time.time()
+f2 = fibonacci_memo(40)
+end_time = time.time()
+print(f"f2 = {f2}, Elapsed: {(end_time - start_time):.8f} seconds")
